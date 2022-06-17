71bab797dd6543bbe2ead21cc7c66738d9ef2f20
LM3ALLEM <59332610+lucas23456@users.noreply.github.com>
2022-06-17 18:24:48 +0300

    commit

---
+++ public/chat.js
@@ -0,0 +1,72 @@
+$(function() {
+  var socket = io.connect("http://localhost:4000");
+
+  var message = $("#message");
+  var username = $("#username");
+  var send_message = $("#send_message");
+  var send_username = $("#send_username");
+  var chatroom = $("#chatroom");
+  var feedback = $("#feedback");
+
+  send_message.click(() => {
+    socket.emit("new_message", {
+      message: message.val(),
+      className: alertClass
+    });
+  });
+  var min = 1;
+  var max = 6;
+  var random = Math.floor(Math.random() * (max - min)) + min;
+
+  // Устаналиваем класс в переменную в зависимости от случайного числа
+  // Эти классы взяты из Bootstrap стилей
+  var alertClass;
+  switch (random) {
+    case 1:
+      alertClass = "secondary";
+      break;
+    case 2:
+      alertClass = "danger";
+      break;
+    case 3:
+      alertClass = "success";
+      break;
+    case 4:
+      alertClass = "warning";
+      break;
+    case 5:
+      alertClass = "info";
+      break;
+    case 6:
+      alertClass = "light";
+      break;
+  }
+
+  socket.on("add_mess", data => {
+    feedback.html("");
+    message.val("");
+    chatroom.append(
+      "<div class='alert alert-" +
+        data.className +
+        "'<b>" +
+        data.username +
+        "</b>: " +
+        data.message +
+        "</div>"
+    );
+  });
+
+  send_username.click(() => {
+    socket.emit("change_username", { username: username.val() });
+  });
+
+  message.bind("keypress", () => {
+    socket.emit("typing");
+  });
+
+  socket.on("typing", data => {
+    feedback.html(
+      "<p><i>" + data.username + " печатает сообщение..." + "</i></p>"
+    );
+  });
+});
